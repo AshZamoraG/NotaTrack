1c5e4fe7894199119f4063b91a417004a112edbb
208040242 <kperezs@est.utn.ac.cr>
2026-02-04 23:34:54 -0600

    feat: creación módulo usuario

---
+++ src/app/ui/components/usuario-card/usuario-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usuario-card',
+  templateUrl: './usuario-card.component.html',
+  styleUrls: ['./usuario-card.component.scss'],
+})
+export class UsuarioCardComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
