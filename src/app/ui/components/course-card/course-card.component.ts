906e5bebbdf52c26daf429ee695cdb1b0c56b34e
AshZamoraG <145407177+AshZamoraG@users.noreply.github.com>
2026-02-04 21:42:14 -0600

    Se agregaron primeros cursos demo y dashbord

--- src/app/ui/components/course-card/course-card.component.ts
+++ src/app/ui/components/course-card/course-card.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IonicModule }from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+export type CursoMock={
+id: number;
+nombre: string;
+codigo?:string;
+creditos?: number;
+progreso?: number;
+estado?: 'Al día'|'En riesgo'|'Aprobado';
+};
+
+
 
 @Component({
   selector: 'app-course-card',
   templateUrl: './course-card.component.html',
   styleUrls: ['./course-card.component.scss'],
+  standalone: true,
+  imports:[IonicModule, CommonModule, RouterModule],
 })
-export class CourseCardComponent  implements OnInit {
+export class CourseCardComponent  {
 
-  constructor() { }
-
-  ngOnInit() {}
-
+ @Input() curso!: CursoMock;
 }
--- src/app/ui/pages/cursos/cursos.page.ts
+++ src/app/ui/pages/cursos/cursos.page.ts
@@ -2,19 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { CourseCardComponent, CursoMock } from '../../components/course-card/course-card.component';
+
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.page.html',
   styleUrls: ['./cursos.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, CourseCardComponent]
 })
-export class CursosPage implements OnInit {
+export class CursosPage {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  cursos: CursoMock[] = [
+    { id: 1, nombre: 'Matemáticas I', codigo: 'MAT101', creditos: 4, progreso: 45, estado: 'Al día' },
+    { id: 2, nombre: 'Programación II', codigo: 'PROG202', creditos: 3, progreso: 20, estado: 'En riesgo' },
+    { id: 3, nombre: 'Bases de Datos', codigo: 'BD150', creditos: 4, progreso: 80, estado: 'Aprobado' },
+  ];
 
 }
